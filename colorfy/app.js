b5ecdb9a04601a9f27ddbeb8f04dc421eb33a9ec
Brian Watterson <briansw@gmail.com>
2017-06-21 15:12:11 -0400

    upadte colorfy

--- colorfy/app.js
+++ colorfy/app.js
@@ -1,9 +1,17 @@
-colorify({
-  container: 'colorfy-sale-card',
-  accuracy: 10,
-  padding: -1,
-  give: {
-    property: 'background',
-    target: '.sale-card'
-  }
-});
+run_colorfy();
+
+function run_colorfy() {
+  $('.sale-card').each(function(index) {
+    colorify({
+      container: 'colorfy-sale-card-' + index,
+      accuracy: 20,
+      padding: -1,
+      give: {
+        property: 'background',
+        target: 'parent'
+      }
+    });
+  });
+}
+
+
