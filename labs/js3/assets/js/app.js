3a1d05ee5ac889bb9bfcf30116fb4e5da5e75dfd
Brian Watterson <briansw@gmail.com>
2017-10-31 11:28:20 -0400

    js 3

--- labs/js3/assets/js/app.js
+++ labs/js3/assets/js/app.js
@@ -1,26 +1,59 @@
+// click nav-item
+// if statement that checks which button was clicked
+// remove hide class from corresponding button
+
+// $('.nav-item').click(function() {
+//   if ($(this).hasClass('apple-button')) {
+//     $('.apple').removeClass('hide');
+//   } else if ($(this).hasClass('nike-button')) {
+//     $('.nike').removeClass('hide');
+//   } else if ($(this).hasClass('disney-button')) {
+//     $('.disney').removeClass('hide');
+//   } else {
+//     $('.vogue').removeClass('hide');
+//   }
+// });
+
+// $('.nav-item').click(function() {
+//   var target_horse = $(this).data('horse');
+//   $(target_horse).removeClass('hide');
+// });
+
+// $('.nav-item').click(function() {
+//   var target = $(this).data('horse');
+//   var img_to_show;
+
+//   if (target == 'apple') {
+//     img_to_show = '<img class="apple" src="assets/img/apple.png" />';
+//   } else if (target == 'nike') {
+//     img_to_show = '<img class="nike" src="assets/img/nike.png" />';
+//   } else if (target == 'disney') {
+//     img_to_show = '<img class="disney" src="assets/img/disney.png" />';
+//   } else {
+//     img_to_show = '<img class="vogue" src="assets/img/vogue.png" />';
+//   }
+
+//   $('.content').append(img_to_show);
+// });
+
+
 $('.nav-item').click(function() {
-  var button_name = $(this).data('button');
+  var img         = $(this).data('img');
+  var class_name  = $(this).data('classname');
+  var img_to_show = '<div class="' + class_name + ' logo"><img src="assets/img/' + img + '" /></div>';
 
-  $('.content').attr('class', 'column content');
-  $('.content').addClass(button_name);
-
-  $('.nav-item').removeClass('active');
-  $('.about-text').addClass('hide');
-  $(this).addClass('active');
-
-  if (button_name == 'beyonce') {
-    $('.lightbox').removeClass('hide');
-    $('audio')[0].play();
-  } else if (button_name == 'about') {
-    $('.about-text').removeClass('hide');
-  }
+  $('.content').prepend(img_to_show);
 });
 
-$('.close-button').click(function() {
-  $('.lightbox').addClass('hide');
-  $('audio')[0].pause();
-  $('.nav-item').removeClass('active');
-});
+
+
+
+
+
+
+
+
+
 
 
 
