3fa20d5fab92453cfea8a02e5c8f0e283f54667c
Brian Watterson <briansw@gmail.com>
2018-02-13 10:09:52 -0500

    lab 4a

---
+++ labs/lab-4a/assets/js/app.js
@@ -0,0 +1,21 @@
+function hello(name, age) {
+  console.log('Name', name);
+  console.log('Age', age);
+}
+
+hello(987, 17);
+
+
+// these are global variable
+// var first_name = 'Brian',
+//     last_name  = 'Watterson',
+//     full_name  = first_name + ' ' + last_name;
+
+// function hello() {
+//   // this is a local variable
+//   var test = 'Brian Watterson';
+//   alert(full_name);
+// }
+
+// hello();
+
