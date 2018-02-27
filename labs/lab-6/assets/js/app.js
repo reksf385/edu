2e6b32f13ede1fcb8d94685733939be142acfb76
Brian Watterson <briansw@gmail.com>
2018-02-27 11:12:44 -0500

    allow users to remove items from cart

--- labs/lab-6/assets/js/app.js
+++ labs/lab-6/assets/js/app.js
@@ -20,10 +20,11 @@ $('.cart-toggle').click(function() {
 function add_to_cart(line_item) {
   // get img and price of item clicked
   // inject html with img and price
-  var value = $(line_item).data('value'),
-      price = $(line_item).data('price'),
-      img   = $(line_item).data('img'),
-      line_item_html = '<div class="line-item"><div class="line-item-img ' + img + '"></div><div class="line-item-price">' + price + '</div></div>';
+  var value  = $(line_item).data('value'),
+      price  = $(line_item).data('price'),
+      img    = $(line_item).data('img'),
+      target = $(line_item).data('target'),
+      line_item_html = '<div data-target="' + target + '" data-value="' + value + '" class="line-item"><div class="line-item-img ' + img + '"></div><div class="line-item-price">' + price + '</div></div>';
 
   $('.line-items').prepend(line_item_html);
   update_total(value);
@@ -41,3 +42,28 @@ function update_total(line_item_value) {
   $('.total').html(new_price);
   $('.total').data('value', new_value);
 }
+
+$('.line-items').on('click', '.line-item', function() {
+  // remove the line-item that we clicked
+  // decrement the value of total
+  // update the price of total
+  var current_value = $('.total').data('value'),
+      line_item_value = $(this).data('value'),
+      new_value = current_value - line_item_value,
+      new_price = new_value.toLocaleString();
+  
+  $(this).html('').removeClass('line-item');
+  $('.total').html(new_price);
+  $('.total').data('value', new_value);
+
+  // make the shopping item clickable again
+  var shopping_item_class = $(this).data('target');
+  $(shopping_item_class).removeClass('in-cart');
+
+  // decrement the item count in cart
+  var current_item_count = $('.item-count').html(),
+      updated_item_count = current_item_count - 1;
+  
+  $('.item-count').html(updated_item_count);
+  
+});
